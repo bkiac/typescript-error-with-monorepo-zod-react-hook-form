3115189fd36dc07c9fba288df694f22e6bf58bed
Ben Knáb <bkiac@pm.me>
2022-03-30 21:00:51 +0200

    Add forms

--- apps/form-1/pages/index.tsx
+++ apps/form-1/pages/index.tsx
@@ -1,4 +1,16 @@
+import {z} from "zod"
+import {useForm} from "react-hook-form"
+import {zodResolver} from "@hookform/resolvers/zod"
+
+const form = z.object({
+	foo: z.string(),
+	bar: z.string(),
+})
+
 export default function Form1() {
+	useForm({
+		resolver: zodResolver(form),
+	})
 	return (
 		<div>
 			<h1>Form 1</h1>
--- apps/form-2/pages/index.tsx
+++ apps/form-2/pages/index.tsx
@@ -1,7 +1,19 @@
+import {z} from "zod"
+import {useForm} from "react-hook-form"
+import {zodResolver} from "@hookform/resolvers/zod"
+
+const form = z.object({
+	foo: z.string(),
+	bar: z.string(),
+})
+
 export default function Form2() {
+	useForm({
+		resolver: zodResolver(form),
+	})
 	return (
 		<div>
-			<h1>Form2</h1>
+			<h1>Form 1</h1>
 		</div>
 	)
 }
