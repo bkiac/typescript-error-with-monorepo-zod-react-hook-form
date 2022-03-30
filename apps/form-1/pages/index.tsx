75b0805fcde8d49eec64681718f84076913e538f
Ben Knáb <bkiac@pm.me>
2022-03-30 20:44:53 +0200

    Add form packages

---
+++ apps/form-1/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Form1() {
+	return (
+		<div>
+			<h1>Form 1</h1>
+		</div>
+	)
+}
---
+++ apps/form-2/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Form2() {
+	return (
+		<div>
+			<h1>Form2</h1>
+		</div>
+	)
+}
