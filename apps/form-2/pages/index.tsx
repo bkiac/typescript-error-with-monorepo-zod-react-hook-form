f78d44d18aec7db250608324b3194d5268e25907
Ben Knáb <bkiac@pm.me>
2022-03-30 21:25:49 +0200

    Format

--- apps/form-2/pages/index.tsx
+++ apps/form-2/pages/index.tsx
@@ -1,19 +1,19 @@
-import {z} from "zod"
-import {useForm} from "react-hook-form"
-import {zodResolver} from "@hookform/resolvers/zod"
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const form = z.object({
-	foo: z.string(),
-	bar: z.string(),
-})
+  foo: z.string(),
+  bar: z.string(),
+});
 
 export default function Form2() {
-	useForm({
-		resolver: zodResolver(form),
-	})
-	return (
-		<div>
-			<h1>Form 1</h1>
-		</div>
-	)
+  useForm({
+    resolver: zodResolver(form),
+  });
+  return (
+    <div>
+      <h1>Form 1</h1>
+    </div>
+  );
 }
